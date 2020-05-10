da0152beae25c1f02d34cd87d6d097d64a26167e
Roy Surjano <rsurjano@outlook.com>
2020-05-10 18:11:11 -0500

    refactor(struture script): ♻️ updating structure script for folder condicionals

--- 00-INSTALLER/02-SCRIPTS/00-bootstrap.sh
+++ 00-INSTALLER/02-SCRIPTS/00-bootstrap.sh
@@ -5,10 +5,6 @@ print_info ">>> BOOTSTRAPING"
 print_info ">>> CREATING BASE STRUCTURE"
 source ./00-structure.sh
 
-print_info ">>> EXPORTING USER DOTFILES FOLDER"
-export USER_DOTFILE=$(whoami)
-export DOTFILES_DIR="$DOTFILES_DIR/$USER_DOTFILE/dotfiles"
-
 print_info ">>> GOING TO DOTFILES FOLDER"
 cd $DOTFILES_DIR
 
--- 00-INSTALLER/02-SCRIPTS/00-structure.sh
+++ 00-INSTALLER/02-SCRIPTS/00-structure.sh
@@ -1,12 +1,22 @@
 print_info ">>> GETTING USER NAME"
-current_user=$(whoami)
+export USER_DOTFILE=$(whoami)
+
+print_info ">>> EXPORTING USER DOTFILES FOLDER"
+export DOTFILES_DIR="/$USER_DOTFILE/dotfiles"
 
 print_info ">>> CLEANING UP"
 rm -rf "$HOME/dotfiles"
 
-print_info ">>> CREATING FOLDER $current_user IF DOESN'T EXIST"
-[[ ! -d "/$current_user" ]] && mkdir "/$current_user"
+print_info ">>> CREATING FOLDER $USER_DOTFILE IF DOESN'T EXIST"
+[[ ! -d "/$USER_DOTFILE" ]] && mkdir "/$USER_DOTFILE"
 
-chown -R "$current_user:$current_user" "/$current_user"
+print_info ">>> ASSIGNING PERMISSIONS "
+chown -R "$USER_DOTFILE:$USER_DOTFILE" "/$USER_DOTFILE"
+mkdir $DOTFILES_DIR
+
+print_info ">>> COPYING FILE TO $DOTFILES_DIR"
+cd ../../
+PARENT_PATH=$(pwd)
+cp -R $(pwd) $DOTFILES_DIR
 
 print_success ">>> CREATED USER STRUCTURE"
