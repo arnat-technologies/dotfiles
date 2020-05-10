f82e8c63a0127366013569d7405778c629c632dd
Roy Surjano <rsurjano@outlook.com>
2020-05-10 17:53:45 -0500

    refactor(structure script): ♻️ updating structure script0

--- 00-INSTALLER/02-SCRIPTS/00-structure.sh
+++ 00-INSTALLER/02-SCRIPTS/00-structure.sh
@@ -1,11 +1,11 @@
 # GETTING USER NAME
-local current_user=$(whoami)
+current_user=$(whoami)
 
 # CLEANING UP
-rm -rf "/$current_user/dotfiles"
+rm -rf "$HOME/dotfiles"
 
 # CREATING / ASSIGNING FOLDER PERMISSIONS
 mkdir "/$current_user"
-chown -R $current_user:users "/$current_user"
+chown -R "$current_user:$current_user" "/$current_user"
 
 print_success ">>> CREATED USER STRUCTURE"
