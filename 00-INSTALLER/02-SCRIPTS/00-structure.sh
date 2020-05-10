24bd6b894b5f7ff883ade546b7c5634a321aa476
Roy Surjano <rsurjano@outlook.com>
2020-05-10 17:40:53 -0500

    fix(sudo for script): 🐛 removed sudo for script iteration

--- 00-INSTALLER/02-SCRIPTS/00-structure.sh
+++ 00-INSTALLER/02-SCRIPTS/00-structure.sh
@@ -5,7 +5,7 @@ local current_user=$(whoami)
 rm -rf "/$current_user/dotfiles"
 
 # CREATING / ASSIGNING FOLDER PERMISSIONS
-sudo mkdir "/$current_user"
-sudo chown -R $current_user:users "/$current_user"
+mkdir "/$current_user"
+chown -R $current_user:users "/$current_user"
 
 print_success ">>> CREATED USER STRUCTURE"
