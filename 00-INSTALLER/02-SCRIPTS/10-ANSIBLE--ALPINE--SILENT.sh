8210550b8c7412c8f46303f8c51d3e9621d281b8
Roy Surjano <rsurjano@outlook.com>
2020-05-10 19:40:34 -0500

    refactor(vscode-environment): ♻️ adding vscode environment for contianers

--- 00-INSTALLER/02-SCRIPTS/10-ANSIBLE--ALPINE--SILENT.sh
+++ 00-INSTALLER/02-SCRIPTS/10-ANSIBLE--ALPINE--SILENT.sh
@@ -1,5 +1,12 @@
-export SILENT_INSTALL=true
+if [ -z "$VSCODE_CONTAINER" ]
+then
+    print_info ">>> NOT IS A VSCODE CONTANER ENVIRONMENT"
+else
+    print_info ">>> VSCODE ENVIRONMENT DETECTED"
+    cd "$(pwd)/00-INSTALLER/02-SCRIPTS"
+fi
 
-source "$(pwd)/01-INSTALLER--ALPINE.sh"
+export SILENT_INSTALL=true
+source "./01-INSTALLER--ALPINE.sh"
 
 print_success ">>> BOOTSTRAPING FINALIZED..."
