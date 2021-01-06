f7da0bf9a922d82f4bef7c049068f043ef0cc138
Roy Surjano <roy@surjano.xyz>
2021-01-06 16:44:37 -0500

    updating variables

--- .scripts/00-ENVIRONMENT.sh
+++ .scripts/00-ENVIRONMENT.sh
@@ -4,5 +4,5 @@ export DOTFILES_DIR="$HOME/.user/dotfiles"
 export SCRIPTS_DIR="$DOTFILES_DIR/.scripts"
 
 echo ">>> Linking Files & Setting initial variables"
-sh "$HOME/.user/dotfiles/.scripts/01-config.sh"
-sh "$HOME/.user/dotfiles/.scripts/02-bootstrap.sh"
+sh "$SCRIPTS_DIR/01-config.sh"
+sh "$SCRIPTS_DIR/02-bootstrap.sh"
