7bdea90ed5f00545afa13e09ca488d45a456c2c0
Roy Surjano <roy@surjano.xyz>
2021-01-06 16:59:56 -0500

    commenting symlink

--- .scripts/02-bootstrap.sh
+++ .scripts/02-bootstrap.sh
@@ -3,6 +3,6 @@
 echo ">>> BOOTSTRAPPING"
 
 sh "$SCRIPTS_DIR/03-helpers.sh"
-sh "$SCRIPTS_DIR/04-symlink-files.sh"
+# sh "$SCRIPTS_DIR/04-symlink-files.sh"
 
 print_success ">>> READY FOR INSTALLING..."
