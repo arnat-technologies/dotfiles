12b7de33c3d73bfa65c12054648ae7380876af24
Roy Surjano <roy@surjano.xyz>
2021-01-06 15:44:47 -0500

    chaing bash

--- .scripts/00-ENVIRONMENT.sh
+++ .scripts/00-ENVIRONMENT.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 echo ">>> Linking Files & Setting initial variables"
-source "~/.user/dotfiles/.scripts/01-config.sh"
-source "~/.user/dotfiles/.scripts/02-bootstrap.sh"
+sh "~/.user/dotfiles/.scripts/01-config.sh"
+sh "~/.user/dotfiles/.scripts/02-bootstrap.sh"
