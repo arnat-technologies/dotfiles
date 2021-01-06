fdeef931d22b33f08d7e618155794e2fcf48e733
Roy Surjano <roy@surjano.xyz>
2021-01-06 18:50:39 -0500

    testing environment

--- .scripts/00-ENVIRONMENT.sh
+++ .scripts/00-ENVIRONMENT.sh
@@ -4,9 +4,14 @@ export DOTFILES_DIR="$TARGET_HOME/.user/dotfiles"
 export SCRIPTS_DIR="$DOTFILES_DIR/.scripts"
 export SILENT_INSTALL=true
 
+echo "TARGET_HOME $TARGET_HOME"
+echo "DOTFILES_DIR $DOTFILES_DIR"
+echo "SCRIPTS_DIR $SCRIPTS_DIR"
+echo "SILENT_INSTALL $SILENT_INSTALL"
+
 echo ">>> Linking Files & Setting initial variables"
 . "$SCRIPTS_DIR/03-helpers.sh"
-. "$SCRIPTS_DIR/04-symlink-files.sh"
+# . "$SCRIPTS_DIR/04-symlink-files.sh"
 
 print_success ">>> READY FOR INSTALLING..."
 
