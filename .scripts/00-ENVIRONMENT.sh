8e0a207c7cb5d80899959bd0fa215ec8ba1d6c22
Roy Surjano <roy@surjano.xyz>
2021-01-06 16:23:32 -0500

    refactoring variables

--- .scripts/00-ENVIRONMENT.sh
+++ .scripts/00-ENVIRONMENT.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
+echo ">>> EXPORTING VARIABLES"
+export DOTFILES_DIR="~/.user/dotfiles"
+export SCRIPTS_DIR="$DOTFILES_DIR/.scripts"
 
 echo ">>> Linking Files & Setting initial variables"
-sh "~/.user/dotfiles/.scripts/01-config.sh"
-sh "~/.user/dotfiles/.scripts/02-bootstrap.sh"
+sh "$HOME/.user/dotfiles/.scripts/01-config.sh"
+sh "$HOME/.user/dotfiles/.scripts/02-bootstrap.sh"
