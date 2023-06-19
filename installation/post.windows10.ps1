5c13f03e356d85c371a0c26527617327dc29da0f
Roy Surjano <roy.surjano@arnat.digital>
2023-06-19 17:09:15 +0000

    updated remoted todo for windows 10

--- installation/post.windows10.ps1
+++ installation/post.windows10.ps1
@@ -4,3 +4,12 @@ Set-ExecutionPolicy Unrestricted
 (Get-WmiObject Win32_ComputerSystem).Rename("MyMachineName") | Out-Null
 
 # Move Personal folders to external drive: Music, Photos, Downloads, etc
+
+# TODO
+
+- [ ] Migrate Spotify to external HDD
+- [ ] Migrate Docker to external HDD
+- [ ] Migrate Google Drive to external HDD
+- [ ] Migrate Onedrive to extenral Drive
+- [ ] Create folder for OpenSource Software for external drive
+- [ ] Migrate Installable Windows software to external drive
